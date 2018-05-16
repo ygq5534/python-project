9a88b3eb61276331c0be6add019b0004efc9bf30
ygq <ygq_5534@163.com>
2018-05-16 22:04:47 +0800

    add ipython_pandas_test

---
+++ pig_peiqi/aixin.py
@@ -0,0 +1,48 @@
+import turtle
+import time
+
+
+def liujia():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+
+turtle.color('red', 'pink')
+turtle.pensize(2)
+turtle.speed(10)
+turtle.goto(0, 0)
+
+turtle.begin_fill()
+turtle.left(140)
+turtle.forward(112)
+liujia()
+turtle.left(120)
+liujia()
+turtle.forward(112)
+turtle.end_fill()
+turtle.pensize(5)
+turtle.up()
+turtle.goto(-50, 142.7)
+turtle.left(50)
+turtle.down()
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(25)
+turtle.up()
+turtle.goto(37.5, 142.7)
+turtle.down()
+turtle.forward(25)
+turtle.up()
+turtle.goto(50, 142.7)
+turtle.right(90)
+turtle.down()
+turtle.forward(60)
+
+for i in range(20):
+    turtle.right(7.8)
+    turtle.forward(0.3)
+turtle.forward(8)
+turtle.up()
+turtle.goto(100, -10)
+turtle.write("I Love you")
---
+++ pig_peiqi/turtle_test.py
@@ -0,0 +1,7 @@
+import turtle
+import time
+turtle.color("red")
+for i in range(40):
+    turtle.forward(200)
+    turtle.right(110)  # 右转角度
+time.sleep(5)
